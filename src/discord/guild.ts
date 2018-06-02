2d0cbe6098f146992d03ab2a827b65dd7a511421
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-02 23:28:36 +0300

    Fixed lint error

--- src/discord/guild.ts
+++ src/discord/guild.ts
@@ -2,7 +2,7 @@ export function getGuildInfo(app: any, client: any, CHANNEL_NAME: string) {
   client.on('ready', () => {
     var guilds = client.guilds.array(),
       membersCount = 0;
-    guilds.forEach(item => {
+    guilds.forEach((item: any) => {
       if (item.name === CHANNEL_NAME) {
         membersCount = item.memberCount;
         app.get('/', function(req: any, res: any) {
