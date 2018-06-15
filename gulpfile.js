bb251805c61e6b91c9317fae7c1700dc4cc7cda2
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-16 00:21:17 +0300

    Finalise dev & build scripts setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 var gulp = require('gulp'),
   validator = require('gulp-html'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  shell = require('gulp-shell');
 
 gulp.task('html', function() {
   return gulp.src('src/chat-form/index.html').pipe(validator()).pipe(gulp.dest('dist/server/chat-form/'));
@@ -12,4 +13,12 @@ gulp.task('sass', function() {
     .src('src/chat-form/form.sass')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('dist/server/chat-form/'));
+});
+
+gulp.task('compileTS', shell.task('./node_modules/.bin/tsc'));
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.sass', ['sass']);
+  gulp.watch('src/chat-form/index.html', ['html']);
+  gulp.watch('src/**/*.ts', ['compileTS']);
 });
