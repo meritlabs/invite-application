fe350e0c0f43a4154ba70b17d358f0d70c8d6c77
Vladimir Gutorov <vlgutv@gmail.com>
2018-06-26 01:02:01 +0300

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+
+
+while true; do
+        read -p "Would you like to deploy to Merit BOT to production? [Y/N]: " yn
+        case $yn in
+            [Yy]* ) git checkout master && heroku git:remote -a merit-invite-discord-bot && git push heroku your_branch:master; break;;
+            [Nn]* ) heroku git:remote -a test-merit-invite-discord-bot && echo 'Enter current branch below' && read branch && git push heroku $branch:master; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
